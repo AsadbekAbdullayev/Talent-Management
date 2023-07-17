9866b381c2f545255c8ce87a9da0ac24b849aed7
AsadbekAbdullayev <asadbekmmm127@gmail.com>
2023-07-17 17:07:32 +0500

    test 15

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test 12
+          Test 15
         </p>
         <a
           className="App-link"
